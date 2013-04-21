921b3ead3ca264ac55d394da933409db8f8064a1
Craig Wattrus <cwattrus@gmail.com>
2013-04-21 13:18:14 +0200

    Craig: Adding in js to get average image color, use getAverageRgb(<image element>) code found at http://stackoverflow.com/questions/11867545/change-text-color-based-on-brightness-of-the-covered-background-area?lq=1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,45 @@
+function getAverageRGB(imgElement) {
 
+    var blockSize = 5, // only visit every 5 pixels
+        defaultRGB = {r:0,g:0,b:0}, // for non-supporting envs
+        canvas = document.createElement('canvas'),
+        context = canvas.getContext && canvas.getContext('2d'),
+        data, width, height,
+        i = -4,
+        length,
+        rgb = {r:0,g:0,b:0},
+        count = 0;
+
+    if (!context) {
+        return defaultRGB;
+    }
+
+    height = canvas.height = imgElement.naturalHeight || imgElement.offsetHeight || imgElement.height;
+    width = canvas.width = imgElement.naturalWidth || imgElement.offsetWidth || imgElement.width;
+
+    context.drawImage(imgElement, 0, 0);
+
+    try {
+        data = context.getImageData(0, 0, width, height);
+    } catch(e) {
+        /* security error, img on diff domain */
+        return defaultRGB;
+    }
+
+    length = data.data.length;
+
+    while ( (i += blockSize * 4) < length ) {
+        ++count;
+        rgb.r += data.data[i];
+        rgb.g += data.data[i+1];
+        rgb.b += data.data[i+2];
+    }
+
+    // ~~ used to floor values
+    rgb.r = ~~(rgb.r/count);
+    rgb.g = ~~(rgb.g/count);
+    rgb.b = ~~(rgb.b/count);
+
+    return rgb;
+
+}
